054c3201e1647db4aeb6c3e79fdd646d01202049
Dongming Xia <dongmingxia@DongmingdeMacBook-Pro.local>
2022-02-15 00:01:16 +0800

    add friendship memcached

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from friendships.models import Friendship
+from friendships.services import FriendshipService
 from friendships.api.serializers import (
     FollowingSerializer,
     FollowerSerializer,
@@ -61,6 +62,8 @@ class FriendshipViewSet(viewsets.GenericViewSet):
                 'errors': serializer.errors,
             }, status=status.HTTP_400_BAD_REQUEST)
 
+        # Clear the cache when there is a follow action
+        FriendshipService.invalidate_following_cache(request.user.id)
         # save() will call create() method in FriendshipSerializerForCreate
         serializer.save()
 
@@ -81,6 +84,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             from_user=request.user,
             to_user=pk,
         ).delete()
+        FriendshipService.invalidate_following_cache(request.user.id)
         return Response({'success': True, 'deleted': deleted})
 
     def list(self, request):
--- friendships/services.py
+++ friendships/services.py
@@ -1,4 +1,9 @@
+from django.conf import settings
+from django.core.cache import caches
 from friendships.models import Friendship
+from twitter.cache import FOLLOWINGS_PATTERN
+
+cache = caches['testing'] if settings.TESTING else caches['default']
 
 
 class FriendshipService(object):
@@ -39,8 +44,23 @@ class FriendshipService(object):
         return [friendship.from_user for friendship in friendships]
 
     @classmethod
-    def has_followed(cls, from_user, to_user):
-        return Friendship.objects.filter(
-            from_user=from_user,
-            to_user=to_user,
-        ).exists()
+    def get_following_user_id_set(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        user_id_set = cache.get(key)
+        if user_id_set is not None:
+            return user_id_set
+
+        friendships = Friendship.objects.filter(from_user_id=from_user_id)
+        user_id_set = set([
+            fs.to_user_id
+            for fs in friendships
+        ])
+        cache.set(key, user_id_set)
+        return user_id_set
+
+    # 如果数据库出现了更新，一般会让缓存失效而不是去更新缓存
+    # 如果去更新cache的话，那么在高并发的场景下有可能会导致数据的不一致
+    @classmethod
+    def invalidate_following_cache(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        cache.delete(key)
